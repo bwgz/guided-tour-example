0efa5e537e14e60a0689289da5139b23c965124d
Bruce Green <bwgz01@gmail.com>
2024-02-16 09:05:26 -0600

    save

---
+++ src/components/GuidedTour.vue
@@ -0,0 +1,85 @@
+<!-- eslint-disable @typescript-eslint/no-empty-function -->
+
+<script setup lang="ts">
+import { computed, defineProps, onMounted, ref, watch } from "vue";
+import GuidedTourStep from "./GuidedTourStep.vue";
+
+const mounted = ref(false);
+
+const props = defineProps({
+    name: String,
+    tour: {
+        type: Object,
+        default: () => {},
+    },
+});
+
+let index = ref(0);
+const isFirst = computed(() => index.value === 0);
+const isLast = computed(() => index.value === props.tour.steps.length - 1);
+
+const step = computed(() => props.tour.steps[index.value]);
+const target = computed(() => step.value?.target);
+
+const next = (payload: MouseEvent): void => {
+    console.log("tour: next", payload);
+    index.value++;
+};
+
+const previous = (payload: MouseEvent): void => {
+    console.log("tour: previous", payload);
+    index.value--;
+};
+
+const finish = (payload: MouseEvent): void => {
+    console.log("tour: finish", payload);
+    index.value = props.tour.steps.length;
+};
+
+watch(target, (value) => {
+    console.log("tour: target", value);
+});
+
+onMounted(() => {
+    console.log("tour: mounted");
+    mounted.value = true;
+}); 
+
+</script>
+
+<template>
+    <Teleport v-if="mounted" :to="target">
+        <div class="v-tour">
+            <GuidedTourStep
+                v-if="tour.steps[index]"
+                :options="tour.options"
+                :step="step"
+                :isFirst="isFirst"
+                :isLast="isLast"
+                :next="next"
+                :previous="previous"
+                :finish="finish"
+            ></GuidedTourStep>
+        </div>
+    </Teleport>
+</template>
+
+<style scoped>
+body.v-tour--active {
+    pointer-events: none;
+}
+
+.v-tour {
+    pointer-events: auto;
+}
+
+.v-tour__target--highlighted {
+    box-shadow: 0 0 0 4px rgba(0, 0, 0, 0.4);
+    pointer-events: auto;
+    z-index: 9999;
+}
+
+.v-tour__target--relative {
+    position: relative;
+}
+</style>
---
+++ src/components/GuidedTourStep.vue
@@ -0,0 +1,186 @@
+<!-- eslint-disable @typescript-eslint/no-unused-vars -->
+<!-- eslint-disable @typescript-eslint/no-empty-function -->
+<script setup lang="ts">
+import { computed, defineProps, onMounted, ref, watch } from "vue";
+
+const mounted = ref(false);
+
+const props = defineProps({
+    name: String,
+    options: {
+        type: Object,
+        default: () => {},
+    },
+    step: {
+        type: Object,
+        default: (() => {}) as ((payload: MouseEvent) => void) | undefined,
+    },
+    next: {
+        type: Function,
+        default: (): void => {},
+    },
+    previous: {
+        type: Function,
+        default: (): void => {},
+    },
+    finish: {
+        type: Function,
+        default: (): void => {},
+    },
+    isFirst: Boolean,
+    isLast: Boolean,
+});
+
+const target = computed(() => mounted.value && document.querySelector(props.step.target));
+
+const next = (payload: MouseEvent): void => {
+    props.next();
+};
+
+const previous = (payload: MouseEvent): void => {
+    props.previous();
+};
+
+const finish = (payload: MouseEvent): void => {
+    props.finish();
+};
+
+watch(target, (value) => {
+    console.log("target", value);
+    if (value) {
+        value.scrollIntoView({ behavior: "smooth" });
+    }
+});
+
+onMounted(() => {
+    console.log("tour step: mounted");
+    mounted.value = true;
+}); 
+</script>
+
+<template>
+    <div class="v-step">
+        <div v-if="step.title" v-html="step.title" class="v-step__header"></div>
+        <div v-if="step.content" v-html="step.content" class="v-step__content"></div>
+        <div class="v-step__buttons">
+            <button v-if="!isLast" @click.prevent="finish" class="v-step__button v-step__button-skip">
+                {{ options.buttons.skip.label }}
+            </button>
+            <button v-if="!isFirst" @click.prevent="previous" class="v-step__button v-step__button-skip">
+                {{ options.buttons.previous.label }}
+            </button>
+            <button v-if="!isLast" @click.prevent="next" class="v-step__button v-step__button-skip">
+                {{ options.buttons.next.label }}
+            </button>
+            <button v-if="isLast" @click.prevent="finish" class="v-step__button v-step__button-skip">
+                {{ options.buttons.finish.label }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.v-step {
+    background: #50596c; /* #ffc107, #35495e */
+    color: white;
+    max-width: 320px;
+    border-radius: 3px;
+    box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0.1) 0px 4px 6px -1px,
+        rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+    padding: 1rem;
+    pointer-events: auto;
+    text-align: center;
+    z-index: 10000;
+
+    &--sticky {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        & .v-step__arrow {
+            display: none;
+        }
+    }
+}
+
+.v-step__arrow,
+.v-step__arrow::before {
+    position: absolute;
+    width: 10px;
+    height: 10px;
+    background: inherit;
+}
+
+.v-step__arrow {
+    visibility: hidden;
+
+    &--dark {
+        &:before {
+            background: #454d5d;
+        }
+    }
+}
+
+.v-step__arrow::before {
+    visibility: visible;
+    content: "";
+    transform: rotate(45deg);
+    margin-left: -5px;
+}
+
+.v-step[data-popper-placement^="top"] > .v-step__arrow {
+    bottom: -5px;
+}
+
+.v-step[data-popper-placement^="bottom"] > .v-step__arrow {
+    top: -5px;
+}
+
+.v-step[data-popper-placement^="right"] > .v-step__arrow {
+    left: -5px;
+}
+
+.v-step[data-popper-placement^="left"] > .v-step__arrow {
+    right: -5px;
+}
+
+/* Custom */
+
+.v-step__header {
+    margin: -1rem -1rem 0.5rem;
+    padding: 0.5rem;
+    background-color: #454d5d;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+}
+
+.v-step__content {
+    margin: 0 0 1rem 0;
+}
+
+.v-step__button {
+    background: transparent;
+    border: 0.05rem solid white;
+    border-radius: 0.1rem;
+    color: white;
+    cursor: pointer;
+    display: inline-block;
+    font-size: 0.8rem;
+    height: 1.8rem;
+    line-height: 1rem;
+    outline: none;
+    margin: 0 0.2rem;
+    padding: 0.35rem 0.4rem;
+    text-align: center;
+    text-decoration: none;
+    transition: all 0.2s ease;
+    vertical-align: middle;
+    white-space: nowrap;
+
+    &:hover {
+        background-color: rgba(white, 0.95);
+        color: #50596c;
+    }
+}
+</style>
