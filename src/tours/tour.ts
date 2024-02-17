23883b159c1c6aecd6efdaca13c02fc5a020c928
Bruce Green <bwgz01@gmail.com>
2024-02-17 15:16:01 -0600

    save

--- src/tours/tour.ts
+++ src/tours/tour.ts
@@ -1,5 +1,36 @@
+
+export type TourOptions = {
+    buttons: {
+        skip: {
+            label?: string,
+        },
+        previous: {
+            label?: string,
+        },
+        next: {
+            label?: string,
+        },
+        finish: {
+            label?: string,
+        },
+    },
+    arrow?: boolean,
+    beacon?: boolean,
+};
+
+export type TourPlacement = 'top' | 'right' | 'left' | 'bottom';
+
+export type TourStep = {
+    target?: string | HTMLElement,
+    title?: string,
+    content?: string,
+    placement?: TourPlacement,
+    arrow?: boolean,
+    beacon?: boolean,
+};
+
 export type Tour = {
     name: string;
-    options: any;
-    steps: any[];
+    options: TourOptions;
+    steps: TourStep[];
 }
