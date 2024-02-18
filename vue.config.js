d960a4f9ffc56a6774580e0335e27b3d41084e7b
Bruce Green <bwgz01@gmail.com>
2024-02-18 06:25:57 -0600

    simplify

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,4 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     publicPath: process.env.NODE_ENV === "production" ? "/guided-tour-example/" : "/",
     transpileDependencies: true,
-    css: {
-        extract: {
-            filename: "[name].css",
-            chunkFilename: "[name].css",
-        },
-    },
-    configureWebpack: {
-        output: {
-            filename: "[name].js",
-            chunkFilename: "[name].js",
-        },
-    },
 });
