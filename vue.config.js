967fb25c7973ca0a7d9bb5557e8997687deebbb4
Bruce Green <bwgz01@gmail.com>
2024-02-17 18:17:52 -0600

    save

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+    publicPath: process.env.NODE_ENV === "production" ? "/guided-tour-example/" : "http://127.0.0.1:8080",
     transpileDependencies: true,
     css: {
         extract: {
